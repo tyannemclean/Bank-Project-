986dccfa8064f76aa2ce214cfe06a9cf4d14099e
tyannemclean <121858609+tyannemclean@users.noreply.github.com>
2023-05-02 22:37:05 -0400

    Add files via upload
    Added the replit

---
+++ main.py
@@ -0,0 +1,18 @@
+account_number = 23768910
+
+input('Hello, what is your account number?')
+
+if input == 23768910:
+  print ('Correct.');
+  input('What do you need help with? Select 1 to check the balance. Select 2 to chsange password. Select 3 to ')
+else:
+  print ('Incorrect. Please try again, or contact customer support.')
+
+
+#a = 3
+#while a > 4: 
+   #print ('apple');
+#else: 
+ # print ('orange'); 
+
+#THESE ARE IDEAS,NOT THE CODE
